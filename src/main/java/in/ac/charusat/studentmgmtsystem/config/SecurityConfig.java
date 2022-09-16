c0860a16e5217752b7e1e27935bf14e46c8c393f
happy-20it032 <88425441+happy-20it032@users.noreply.github.com>
2022-09-16 12:12:39 +0530

    Update SecurityConfig.java

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package in.ac.charusat.studentmgmtsystem.config;
-
+//
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
